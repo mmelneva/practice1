969fdbb153867cfe295c4c6a79e850b951584e23
Mary Melneva <mmelneva@mail.ru>
2023-05-15 21:00:04 +0300

    final_project

--- projects/Task/app/views/client/catalog_products/index.blade.php
+++ projects/Task/app/views/client/catalog_products/index.blade.php
@@ -9,48 +9,32 @@
 </head>
 <body>
 
-<h1>Список товаров</h1>
-<div class="tab">
+<h1 class="mb-1 mt-1 ml-4">Список товаров</h1>
+<div class="tab mb-2 nav-bar">
+<div class="ml-3">
 @foreach($categoryTree as $category)
 	<button class="btn btn-link" onclick="openCategory(event, '{{{ $category->id }}}')">{{{ $category->name }}}</button>
 @endforeach
 
 </div>
+</div>
 
-<div class="container">
-<div class="row">
+<div class="my-container mx-1">
+<div class="my-row">
 <div class="col-6 main">
 
- 
-
 @foreach($products as $product)
-<div class="product-item" name="{{{ $product->category_id }}}">
+<div class="product-item card mr-1" name="{{{ $product->category_id }}}">
 
-<!--<div class="product-img">
-<a href="">
-<img src="{{{ $product->image }}}">
-</a>
-</div>-->
-
-
-<div class="product-list">
+<div class="card-body">
+<h5 class="card-title">
 @if (strlen($product->name > 50))
-<h3>{{{ mb_substr($product->name, 0, 50) }}}...</h3>
+{{{ mb_substr($product->name, 0, 50) }}}...
 @else
-<h3>{{{ $product->name }}}</h3>
+{{{ $product->name }}}
 @endif
-<!--<div class="stars"></div>-->
-<span class="price"> ₽ {{{ $product->price }}}</span>
-<!--<div class="actions">
-<div class="add-to-cart">
-<a href="" class="cart-button">В корзину</a>
-</div>
-<div class="add-to-links">
-<a href="" class="wishlist"></a>
-<a href="" class="compare"></a>
-</div>
-<div class="id_item">catId => {{{ $product->category_id }}}</div>
-</div>-->
+</h5>
+<h6 class="card-subtitle mb-2 text-muted"> ₽ {{{ $product->price }}}</h6>
 </div>
 </div>
 @endforeach
